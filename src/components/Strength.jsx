3ca3415c73b266b5e40af8083977dae5b29e4141
Renan <renansouza2310@gmail.com>
2023-08-01 11:43:09 -0300

    Implementa a lógica que mostra o texto relativo a força da senha

--- src/components/Strength.jsx
+++ src/components/Strength.jsx
@@ -1,31 +1,56 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const Strength = (props) => {
+  const [passwordStrength, setPasswordStrength] = useState("")
   const password = props.password;
-  let upperCase = false;
-  let lowerCase = false;
-  let number = false;
-  let symbol = false;
-  const [passwordStregnth, setPasswordStrength] = useState("")
 
-  for (let i = 0; i < password.length; i++) {
-    if (!isNaN(password[i])) {
-      number = true;
-    } else if (password[i] == password[i].toLowerCase()) {
-      lowerCase = true;
-    } else if (password[i] == password[i].toUpperCase()) {
-      upperCase = true;
-    } else {
-      symbol = true;
+  useEffect(() => {
+    if (password !== "") {
+      handlePasswordStrength()
     }
+  }, [password])
+
+  const handlePasswordStrength = () => {
+    let power;
+    let character = {
+      upperCase: false,
+      lowerCase: false,
+      number: false,
+      symbol: false
+    }
+
+    for (let i = 0; i < password.length; i++) {
+      if (/[A-Z]/.test(password[i])) {
+        character.upperCase = true
+      } else if (/[a-z]/.test(password[i])) {
+        character.lowerCase = true
+      } else if (/\W/.test(password[i])) {
+        character.symbol = true
+      } else {
+        character.number = true
+      }
+    }
+
+    let differentTypesOfCharacters = Object.values(character).filter(value => value === true).length
+
+    if (password.length > 8 && differentTypesOfCharacters === 4) {
+      power = "STRONG"
+    } else if (password.length > 8 && differentTypesOfCharacters > 2) {
+      power = "MEDIUM"
+    } else if (password.length > 8 && differentTypesOfCharacters === 2) {
+      power = "WEAK"
+    } else {
+      power = "TOO WEAK!"
+    }
+
+    setPasswordStrength(power)
   }
   
-
   return (
     <div className="grid grid-cols-strength place-content-center items-center w-mobile-button bg-custom-black p-4">
         <p className="text-custom-gray w-fit">STRENGTH</p>
         <div className="flex gap-x-2 justify-end items-center">
-          <p className="text-custom-light-gray text-xl">TOO WEAK!</p>
+        <p className="text-custom-light-gray text-xl">{passwordStrength}</p>
           <div className="w-stregth-indicator h-7 border-2"></div>
           <div className="w-stregth-indicator h-7 border-2"></div>
           <div className="w-stregth-indicator h-7 border-2"></div>
